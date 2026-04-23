70d09c6fc4d71395f70e56ffa18fd3cf7b6d01b8
Hazem_Genady <hazem.genady.18@gmail.com>
2026-04-24 02:54:05 +0300

    Finishing Serarch style and functions (cards info aren't complete)

--- js/search.js
+++ js/search.js
@@ -1,15 +1,352 @@
-//بنمسك البوتونز كلها نحطها في ليست
-const buttons = document.querySelectorAll(".filter-method button");
+//list هنحط يخواتي كل الصور ومعلوماتها في 
+//The List contains dictionaries (objects) => (key : value)
+//كل صورة ليها معلومات مختلفة متخزنة هنا
+let products = {
+    data: [
+        {
+            id: 1,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/1.jpg",
+            header: "artifact"
+        }, {
+            id: 2,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/dsc_0040.jpg",
+            header: "artifact"
+        }, {
+            id: 3,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/dsc.jpg",
+            header: "artifact"
+        }, {
+            id: 4,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/g75a3519.jpg",
+            header: "artifact"
+        }, {
+            id: 5,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/page69-1003-full.jpg",
+            header: "artifact"
+        }, {
+            id: 6,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/القبطي-7.jpg",
+            header: "artifact"
+        }, {
+            id: 7,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/القبطي-9.jpg",
+            header: "artifact"
+        }, {
+            id: 8,
+            artifactName: "Test name",
+            category: "Coptic Musuem",
+            image: "../assets/images/coptic museum/القبطي-20.jpg",
+            header: "artifact"
+        }, {
+            id: 9,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/25-1.jpg",
+            header: "artifact"
+        }, {
+            id: 10,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/31-1.jpg",
+            header: "artifact"
+        }, {
+            id: 11,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/32-1-1024x682.jpg",
+            header: "artifact"
+        }, {
+            id: 12,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/32h.png",
+            header: "artifact"
+        }, {
+            id: 13,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/34-1.jpg",
+            header: "artifact"
+        }, {
+            id: 14,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/35-1.jpg",
+            header: "artifact"
+        }, {
+            id: 15,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/40-1.jpg",
+            header: "artifact"
+        }, {
+            id: 16,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/43-1.jpg",
+            header: "artifact"
+        }, {
+            id: 17,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/49-1.jpg",
+            header: "artifact"
+        }, {
+            id: 18,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/54.jpg",
+            header: "artifact"
+        }, {
+            id: 19,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/64.jpg",
+            header: "artifact"
+        }, {
+            id: 20,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/67-819x1024.jpg",
+            header: "artifact"
+        }, {
+            id: 21,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/84.jpg",
+            header: "artifact"
+        }, {
+            id: 22,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/99-819x1024.jpg",
+            header: "artifact"
+        }, {
+            id: 23,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/egyptian_triad.jpg",
+            header: "artifact"
+        }, {
+            id: 24,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/ka-tutankhamun-guard-statue.jpg",
+            header: "artifact"
+        }, {
+            id: 25,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/Tutankhamon-mask.png",
+            header: "artifact"
+        }, {
+            id: 26,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/Yuya-and-Thuya-papyrus-emc-1024x851.jpg",
+            header: "artifact"
+        }, {
+            id: 27,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 28,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 29,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 30,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 31,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 32,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 33,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 34,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 35,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 36,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 37,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 38,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 39,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        }, {
+            id: 40,
+            artifactName: "Test name",
+            category: "Egyptian Museum",
+            image: "../assets/images/egy museum/",
+            header: "artifact"
+        },
+    ]
+}
 
-// loop on this list
-buttons.forEach(button => {
-    button.addEventListener('click', function(){
-       //inner loop if the button was clicked
-        buttons.forEach(btn => {
-            //removing the hover class from all buttons
-            btn.classList.remove('active-method')
-        })
-        //نضيفه للبوتون اللي اتداس بس
-        this.classList.add('active-method')    
+//هنعمل العناصر باستخدام الجافا 
+for (let i of products.data) {//لوب عادية
+    //=> "i" is our counter ---- i.category == the ith value of "category" key in our dictionary(object)   
+
+    let card = document.createElement("div")
+    let x = i.category.replaceAll(" ", "-")//علشان الكاتيجوري اللي في الأوبجيكت فيها مسافات ولو في مسافات بيعتبر الكلمتين اتنين كلاس مختلفين مش كلاس واحد
+    card.classList.add("card", x, "hide") //adding these classes to each card
+    
+    //الديف اللي هيشيل الصورة
+    let imgContainer = document.createElement("div")
+    imgContainer.classList.add("image-container")//كلاس عادي له
+    let image = document.createElement("img")//الصورة نفسها
+    image.setAttribute("src", i.image)//adding src attribute and siiting its value to the image path from the dictionary
+    imgContainer.appendChild(image)//نحط الصورة جوا الديف
+    card.appendChild(imgContainer)//نحط الديف جوا الكارت
+
+    //باقي الكود نفس المنطق
+    let textContainer = document.createElement("div")
+    textContainer.classList.add("text-container")
+
+    let header = document.createElement("h5")
+    header.classList.add("artifact-header")
+    header.innerText = i.header.toUpperCase()
+    textContainer.appendChild(header)
+
+    
+
+    let name = document.createElement("h2")
+    name.classList.add("artifact-name")
+    name.innerText = i.artifactName.toUpperCase() //الاسم  بتاع الاثر بخليه يساوي الاسم اللي في الليست بالترتيب وكابيتال
+    textContainer.appendChild(name)
+
+    let cardFooter = document.createElement("div")
+    cardFooter.classList.add("card-footer")
+
+    let museum = document.createElement("h6")
+    museum.classList.add("artifacte-subtitle")
+    museum.innerText = i.category.toUpperCase()
+    cardFooter.appendChild(museum)
+    
+    let iconBox = document.createElement("div")
+    iconBox.classList.add("icon-box")
+
+    let archiveIcon = document.createElement("i")
+    archiveIcon.classList.add("fa-solid", "fa-box-archive", "archive-icon")//icon from FontAwsome.com
+    iconBox.appendChild(archiveIcon)
+
+    let HeartIcon = document.createElement("i")
+    HeartIcon.classList.add("fa-solid" ,"fa-heart" ,"heart-icon")
+    iconBox.appendChild(HeartIcon)
+
+    cardFooter.appendChild(iconBox)
+    textContainer.appendChild(cardFooter)
+    card.appendChild(textContainer)
+
+
+    document.getElementById("artifacts-container").appendChild(card)
+}
+
+
+function filterArtifacts(value) {//onclick in html file
+    let buttons = document.querySelectorAll(".button-value")
+    buttons.forEach(button => {//changing color of choosing category
+        if (value.toUpperCase() == button.innerText.toUpperCase()) {
+            button.classList.add("active-category")
+        }
+        else {
+            button.classList.remove("active-category")
+        }
     })
+
+    let elements = document.querySelectorAll(".card")
+    elements.forEach(e => {
+        let x = value.replaceAll(" ", "-")//نخلي اسم المتحف زي الكلاس بتاعه نشيل المسافات ونحط شرطة
+        if (value == "All Museums") {
+            e.classList.remove("hide")//display the card
+        }
+
+        else if (e.classList.contains(x)) {
+            e.classList.remove("hide")
+        }
+        else {
+            e.classList.add("hide")//hide the card
+        }
+
+    })
+}
+
+window.onload = () => { filterArtifacts("All Museums") } //التلقائي يبقا اول ما نحمل الصفحة
+
+document.getElementById("search").addEventListener("click", () => {
+    let search = document.getElementById("search-input").value
+    let cards = document.querySelectorAll(".card")
+    let names = document.querySelectorAll(".artifact-name")
+    names.forEach((name, index) => {
+        if (name.innerText.includes(search.toUpperCase()))//هل البحث جواه حروف من اسم اثر معين
+            cards[index].classList.remove("hide")
+        else
+            cards[index].classList.add("hide")
+
+    })
+
 })
